7d6895bbfc9c5a2141185dd36d250e23e7c821ee
Elias Cano <canoeliasnaim@gmail.com>
2023-01-24 15:14:27 -0300

    Proyecto de CV finalizado

---
+++ JS/function.js
@@ -0,0 +1,7 @@
+document.getElementById("Instagram").onclick = function() {
+    window.open('https://www.instagram.com/eliias_cano/?hl=es-la')
+    };
+
+document.getElementById("Mail").onclick = function() {
+    window.open('https://www.mailto:canoeliasnaim@gmail.com')
+    };
